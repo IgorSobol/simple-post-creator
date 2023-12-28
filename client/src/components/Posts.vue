42e8c44137b7de32d0bf4444a91c093d23dacccf
Igor Sobol <sobol.dominus@gmail.com>
2023-12-28 13:12:10 +0100

    Added get from api and post and create post fanctionals

--- client/src/components/Posts.vue
+++ client/src/components/Posts.vue
@@ -7,16 +7,31 @@
       placeholder="Title"
       class="title-input input"
     >
-    <input
-      type="text"
-      v-model="body"
-      placeholder="Body"
-      class="body-input input"
-    >
+    <textarea v-model="body" placeholder="Body" class="body-input input" rows="4" cols="50"></textarea>
 
-    <button v-if="isEditing" @click="updatePost">Update</button>
-    <button v-if="isEditing" @click="cancelEdit">Cancel</button>
-    <button v-else @click="createPost">Create</button>
+    <button class="btn" v-if="isEditing" @click="updatePost">Update</button>
+    <button class="btn" v-if="isEditing" @click="cancelEdit">Cancel</button>
+    <button class="btn" v-else @click="createPost">Create</button>
+
+    <ul class="posts">
+      <li
+        v-for="post in posts"
+        :key="post.id"
+        class="post"
+      >
+      
+        <h2 class="post__title">{{ post.title }}</h2>
+        <hr />
+        <p>{{ post.body }}</p>
+
+        <p class="post__date">{{ post.created_at }}</p>
+
+        <div class="post__footer">
+          <button>Edit</button>
+          <button>Delete</button>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -26,7 +41,14 @@ export default {
   data() {
     return {
       title: "",
+      body: "",
+      isEditing: false,
+      API_URL: "http://localhost:3000/posts",
+      posts: [],
     }
+  },
+  created() {
+    this.getPosts();
   },
   methods: {
     updatePost() {
@@ -35,11 +57,33 @@ export default {
     cancelEdit() {
       return true;
     },
-  },
+    async getPosts() {
+        const data = await fetch(this.API_URL)
+
+        this.posts = await data.json()
+      },
+    },
+    async createPost() {
+      if (!this.title.length || !this.body.length) return;
+
+      const data = await fetch(this.API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: this.title,
+          body: this.body,
+        }),
+      })
+
+      if(data.ok) alert('Post added!')
+      if(!data.ok) alert('Something that wrong!')
+    },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .input {
   width: 100%;
   padding: 12px 20px;
@@ -50,5 +94,41 @@ export default {
   background-color: #f8f8f8;
   box-sizing: border-box;
   resize: vertical;
+}
+
+.btn {
+  width: 100%;
+  margin-top: 20px;
+}
+.posts {
+  list-style: none;
+  padding: 0;
+  margin-top: 20px;
+}
+
+.post {
+  text-align: left;
+  background-color: #313030;
+  padding: 10px;
+  margin-bottom: 15px;
+  border-radius: 6px;
+
+  &__title {
+    margin-top: 0;
+    margin-bottom: 10px;
+  }
+
+  &__date {
+    text-align: end;
+  }
+
+  &__footer {
+    display: flex;
+    justify-content: flex-end;
+
+    button {
+      margin-left: 10px;
+    }
+  }
 }
 </style>
